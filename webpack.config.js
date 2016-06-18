4043ce968a823e843872f2b44d25eaf7270938df
Omid Hezaveh <a@gik.fi>
2016-06-18 09:28:09 +0300

    working webpack react config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 module.exports = {
-    context: __dirname + "/app",
+    context: __dirname ,
     entry: "./index.js",
     output: {
         path: __dirname + "/",
         filename: "bundle.js",
-         loader: ["babel-loader"]
+    },
+module: {
+  loaders: [
+    {
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel', // 'babel-loader' is also a legal name to reference
+      query: {
+        presets: ['react']
+      }
     }
+  ]
 }
+};
