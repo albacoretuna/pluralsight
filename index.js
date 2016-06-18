1ce5675a8b746d6df0f50c1c829cfd102f5e5080
Omid Hezaveh <a@gik.fi>
2016-06-18 10:05:57 +0300

    Simple falcor setup, returns recipes

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 const React = require('react');
 const ReactDom = require('react-dom');
 
+const model = new falcor.Model({
+   cache: {
+       recipes: [
+           {
+               name: "Cookies",
+               instructions: "Bake them lol"
+           },
+           {
+               name: "Brownies",
+               instructions: "Also bake"
+           }
+       ]    
+   }
+});
+
+model.get(['recipes', 0, ['name', 'instruction']])
+     .then(data => {
+         console.log(data);
+     });
+
+ 
+
 const App = React.createClass({
     render() {
         return <h1> hi  </h1>;
